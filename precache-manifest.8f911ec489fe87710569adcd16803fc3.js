30873c3b55b5bfd946bb35b24c8b2fef09fc7d42
MatthewViens <matthew.j.viens@gmail.com>
2019-06-16 10:49:05 -0700

    Updates

---
+++ precache-manifest.8f911ec489fe87710569adcd16803fc3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f200b8225d0eb5b908cc11e4dc3ddb83",
+    "url": "/Dad-Jokes/index.html"
+  },
+  {
+    "revision": "fc33f55c2989bdfc9bf7",
+    "url": "/Dad-Jokes/static/css/main.7a5e56fe.chunk.css"
+  },
+  {
+    "revision": "7ef0a8e378cccc2ee69a",
+    "url": "/Dad-Jokes/static/js/2.6da5c2fe.chunk.js"
+  },
+  {
+    "revision": "fc33f55c2989bdfc9bf7",
+    "url": "/Dad-Jokes/static/js/main.f148893f.chunk.js"
+  },
+  {
+    "revision": "a2f4470fd54387afb7b5",
+    "url": "/Dad-Jokes/static/js/runtime~main.6318a628.js"
+  }
+]);
